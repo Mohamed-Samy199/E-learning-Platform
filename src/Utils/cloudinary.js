e6279a7ca873fc425e2c55c8f6937fd0dbc46c85
Mohamed-Samy199 <https://github.com/Mohamed-Samy199>
2023-11-20 13:22:51 +0200

    E-learing Project

--- src/Utils/cloudinary.js
+++ src/Utils/cloudinary.js
@@ -1,9 +1,7 @@
 import cloudinary from 'cloudinary';
 
 cloudinary.v2.config({
-    cloud_name: 'deeb02zeg',
-    api_key: '565199858344853',
-    api_secret: 'xWqA5pv_j--bsSY_-rEwobHPla0'
+    
 })
 
 export default cloudinary.v2;
